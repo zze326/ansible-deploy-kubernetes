cdfc0a3a0aeac8f0fcdc02a9ebd7932d570ce41a
zze326 <zhangzhongen326@gmail.com>
2021-04-14 09:54:43 +0800

    enhance: 支持部署 kubernetes 1.20+ 版本

--- roles/gen_certs/templates/kubernetes/gen_certs.sh
+++ roles/gen_certs/templates/kubernetes/gen_certs.sh
@@ -2,4 +2,6 @@
 {{ cfssl_bin }} gencert -ca=ca.pem -ca-key=ca-key.pem -config=json_file/ca-config.json -profile=kubernetes json_file/server-csr.json | {{ cfssl_json_bin }} -bare server
 {{ cfssl_bin }} gencert -ca=ca.pem -ca-key=ca-key.pem -config=json_file/ca-config.json -profile=kubernetes json_file/admin-csr.json | {{ cfssl_json_bin }} -bare admin
 {{ cfssl_bin }} gencert -ca=ca.pem -ca-key=ca-key.pem -config=json_file/ca-config.json -profile=kubernetes json_file/kube-proxy-csr.json | {{ cfssl_json_bin }} -bare kube-proxy
+{{ cfssl_bin }} gencert -ca=ca.pem -ca-key=ca-key.pem -config=json_file/ca-config.json -profile=kubernetes json_file/kube-controller-manager-csr.json | {{ cfssl_json_bin }} -bare kube-controller-manager
+{{ cfssl_bin }} gencert -ca=ca.pem -ca-key=ca-key.pem -config=json_file/ca-config.json -profile=kubernetes json_file/kube-scheduler-csr.json | {{ cfssl_json_bin }} -bare kube-scheduler
 {% if ansible_distribution == 'Ubuntu' %}chown {{ exec_user }}.{{ exec_user }} *.pem{% endif %}
